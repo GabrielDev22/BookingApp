959d1b7e1f4405b4b189d9779a24f291c7c8a289
unknown <gabrieltorresmora2@gmail.com>
2023-04-23 19:43:31 -0300

    Creando una aplicacion de reserva

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+export default function LoginPage(){
+    return(
+        <div>
+            <form>
+                <input type="email" placeholder={"your@email.com"}></input>
+                <input type="password" placeholder="password"></input>
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
